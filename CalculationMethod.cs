0ef28d3cdcf413f5efb4e05cefe28cfe65cfaf7c
robbiewang <ouyuchen1999@outlook.com>
2020-10-13 10:09:05 +0800

    修改

---
+++ CalculationMethod.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCalculator
+{
+    class CalculationMethod
+    {
+        private static int result = 0;
+
+        public int Add(int number1, int number2)
+        {
+            result = number1 + number2;
+            return result;
+        }
+
+        public int Subtraction(int number1, int number2)
+        {
+            result = number1 - number2;
+            return result;
+        }
+
+        public int Multiplication(int number1, int number2)
+        {
+            result = number1 * number2;
+            return result;
+        }
+
+        public int Division(int number1, int number2)
+        {
+            result = number1 / number2;
+            return result;
+        }
+        public int remainder(int number1,int number2)
+            {
+            result = number1 % number2;
+            return result;
+
+            }
+        
+            public int Bitwise(int number1, int number2)
+        {
+            result = number1 &number2;
+            return result;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCalculator
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                CalculationMethod cm = new CalculationMethod();
+                Console.Write("请输入两个整数：\n");
+                Console.Write("第一个：");
+                int number1 = int.Parse(Console.ReadLine());
+                Console.Write("第二个：");
+                int number2 = int.Parse(Console.ReadLine());
+                Equals(number1, number2);
+                Console.Write("\n");
+                Console.Write("请输入运算符：\n");
+                string str = Console.ReadLine();
+                switch (str)
+                {
+                    case "+":
+                        Console.Write("结果为：" + cm.Add(number1, number2));
+                        break;
+                    case "-":
+                        Console.Write("结果为：" + cm.Subtraction(number1, number2));
+                        break;
+                    case "*":
+                        Console.Write("结果为：" + cm.Multiplication(number1, number2));
+                        break;
+                    case "/":
+                        Console.Write("结果为：" + cm.Division(number1, number2));
+                        break;
+                    case "%":
+                        Console.Write("结果为：" + cm.Division(number1, number2));
+                        break;
+                    case "&":
+                        Console.Write("结果为：" + cm.Division(number1, number2));
+                        break;
+                }
+                Console.Write("\n");
+                Console.Write("按T键退出，按其他键继续 ");
+                if (Console.ReadKey().KeyChar == 'T')
+                    break;
+                else
+                    Console.Clear();
+                
+            }
+            Console.Read();
+        }
+
+        public static void Equals(int num1, int num2)
+        {
+            if (num1 == num2)
+                Console.Write("输入的两个整数相等\n");
+            else
+                Console.Write("输入的两个整数不相等\n");
+        }
+
+
+
+    }
+}
